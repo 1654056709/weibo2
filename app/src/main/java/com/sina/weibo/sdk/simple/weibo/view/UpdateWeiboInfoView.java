a50c64a7929b4c6c60b62e3f95c69b7d7e3f4e67
john <liluping789@gmail.com>
2017-04-09 21:15:23 +0800

    UpdateWeiboInfoView

---
+++ app/src/main/java/com/sina/weibo/sdk/simple/weibo/view/UpdateWeiboInfoView.java
@@ -0,0 +1,13 @@
+package com.sina.weibo.sdk.simple.weibo.view;
+
+import com.sina.weibo.sdk.simple.weibo.model.UpdateWeiboInfo;
+
+/**
+ * Created by John on 2017/4/9.
+ */
+
+public interface UpdateWeiboInfoView extends View {
+    void onSuccess(UpdateWeiboInfo updateWeiboInfo);
+
+    void onFailure(String errorMsg);
+}
