ab16f4d14e7ef57f6b6d55d62b94786a8f5c0666
john <liluping789@gmail.com>
2017-04-09 21:17:02 +0800

    UpdateWeiboInfoPresenter

---
+++ app/src/main/java/com/sina/weibo/sdk/simple/weibo/presenter/UpdateWeiboInfoPresenter.java
@@ -0,0 +1,29 @@
+package com.sina.weibo.sdk.simple.weibo.presenter;
+
+import com.sina.weibo.sdk.simple.weibo.view.View;
+
+/**
+ * Created by John on 2017/4/9.
+ */
+
+public class UpdateWeiboInfoPresenter implements Presenter {
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onAttachView(View view) {
+
+    }
+}
